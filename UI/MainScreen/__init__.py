3721dff028a6885fdfb69cd59d4b9749cf1323d9
Ali Fartout <alifa2080@gmail.com>
2022-03-29 15:15:54 +0430

    #TODO: Control Bar - Playlist - setting - AI

--- UI/MainScreen/__init__.py
+++ UI/MainScreen/__init__.py
@@ -1,23 +1,28 @@
 import sys
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import *
 from UI.Setting.Setting import Settings
 from UI.AI.AIPage import AIPage
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtMultimediaWidgets import *
+
 
 class App(QMainWindow):
-    def __init__(self,):
+    def __init__(self, ):
         super().__init__()
         self.UIinit()
         self.MenuBar()
+        self.VideoPlayer()
 
     """ initializing title and size of main screen """
+
     def UIinit(self):
         self.setGeometry(0, 0, 1300, 900)
         self.setWindowTitle("Bot PLayer")
 
-
     """ Create MenuBar """
+
     def MenuBar(self):
         # Create Open File action
         openAction = QAction('&Open File', self)
@@ -43,24 +48,121 @@ class App(QMainWindow):
         fileMenu.addAction(settingAction)
         fileMenu.addAction(AIAction)
 
+
+
+    def VideoPlayer(self):
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        videoWidget = QVideoWidget()
+
+        self.playlist = QMediaPlaylist()
+        self.mediaPlayer.setPlaylist(self.playlist)
+
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+        self.stopButton = QPushButton()
+        self.stopButton.setEnabled(False)
+        self.stopButton.setIcon(self.style().standardIcon(QStyle.SP_MediaStop))
+        self.stopButton.clicked.connect(self.stop)
+
+        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+
+        self.volumeSlider = QSlider(Qt.Horizontal)
+        self.volumeSlider.setRange(0, 100)
+        self.volumeSlider.sliderMoved.connect(lambda x=self.volumeSlider.value(): self.setVolume(x))
+
+        self.error = QLabel()
+        self.error.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+
+        # Create a widget for window contents
+        wid = QWidget(self)
+        self.setCentralWidget(wid)
+
+        # Create layouts to place inside widget
+        controlLayout = QGridLayout()
+        controlLayout.addWidget(self.positionSlider, 0, 0)
+        controlLayout.addWidget(self.playButton, 1, 0)
+        controlLayout.addWidget(self.stopButton, 1, 1)
+        controlLayout.addWidget(self.volumeSlider, 1, 2)
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addWidget(videoWidget)
+        #mainLayout.addWidget(self.playlist)
+
+        layout = QVBoxLayout()
+        layout.addLayout(mainLayout)
+        layout.addLayout(controlLayout)
+
+        # Set widget to contain window contents
+        wid.setLayout(layout)
+
+        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+        self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
+
     """ Calling Functions """
+
+    def exitCall(self):
+        sys.exit(App.exec_())
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+
+    def stop(self):
+        self.mediaPlayer.stop()
+
+
+    def mediaStateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(
+                self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(
+                self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def positionChanged(self, position):
+        self.positionSlider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        self.error.setText("Error: " + self.mediaPlayer.errorString())
+
+    def setVolume(self, volume):
+        self.mediaPlayer.setVolume(volume)
+
     # Calling Open File
     def openCall(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self, "QFileDialog.getOpenFileNames()", "",
-                                                     "All Files (*);;Python Files (*.py)", options=options)
-        if files:
-            print(files)
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie",
+                                                  QDir.homePath())
+
+        if fileName != '':
+            self.mediaPlayer.setMedia(
+                QMediaContent(QUrl.fromLocalFile(fileName)))
+            self.playButton.setEnabled(True)
+
     # Calling AI page
     def AICall(self):
         self.AIPage = AIPage()
         self.AIPage.show()
         # QMessageBox.critical(self,"Error", "Playlist is empty.")
+
     # Calling Setting Page
     def settingCall(self):
         self.setting = Settings()
         self.setting.show()
-
-
-
